99f086928bca71b6fe0915150a58be52c6ec2b1c
salimtohtobaev <salim09291610@gmail.com>
2026-06-11 12:30:56 +0500

    added if_else sector

---
+++ Practise2/if_else/if_else.py
@@ -0,0 +1,22 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+
+username = "Emil"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
